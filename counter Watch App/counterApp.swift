c74d82d7d5ea75b380e203bc265bacfa62aef55f
Lukas Valine <lukas@valine.io>
2022-11-17 13:08:36 -0600

    Initial Commit

---
+++ counter Watch App/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter Watch App
+//
+//  Created by Valine, Lukas on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct counter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Valine, Lukas on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
